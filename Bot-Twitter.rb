b488d9c913e7e7667ea5b220e93fd917ba976d96
MaximeBrandely <maxime.brandely@gmail.com>
2018-03-06 20:23:34 +0100

    first

---
+++ Bot-Twitter.rb
@@ -0,0 +1,19 @@
+
+#require 'dotenv'
+require 'twitter'
+
+Dotenv.load
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = "KEY"
+  config.consumer_secret = "KEY"
+  config.access_token = "KEY"
+  config.access_token_secret = "KEY"
+end
+
+to_spam = ["@personne_1", "@personne_2"]
+
+to_spam.each do |name|
+  client.update("Hey #{name}, j'espère que vous allez bien !")
+  sleep(30)
+end
